5189d6f7e2eafb1ce99131cec6e8201fdede5abe
Aggrathon <antonbjo@gmail.com>
2019-04-27 09:58:49 +0300

    Asteroid Colors and entities

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class AsteroidSpawner : MonoBehaviour
 {
-    [System.Serializable]
-    public struct ResourceColor {
-        public Asteroid.Resource resource;
-        public Color color;
-    }
 
     public Transform planet;
     public float atmosphere = 2f;
@@ -16,31 +11,29 @@ public class AsteroidSpawner : MonoBehaviour
     public float height = 5;
     public float density = 1;
     public float rnd = 0.8f;
-    public GameObject prefab;
-
-    public ResourceColor[] colors;
+    public GameObject[] prefabs;
 
     [ContextMenu("Spawn Asteroids")]
     public void SpawnAsteroids() {
         while(true) {
-            var go = GameObject.Find("Asteroid(Clone)");
+            var go = GameObject.Find("Asteroid (Spawned)");
             if (go) DestroyImmediate(go);
             else break;
         }
-        for (float i = density/2; i < width; i += density)
+        for (float j = density/2; j < height; j += density)
         {
-            for (float j = density/2; j < height; j += density)
+            float offset = Random.Range(-density, density) * rnd * 2;
+            for (float i = density/2; i < width; i += density)
             {
                 for (float k = -1; k <= 1; k +=2)
                 {
                     for (float l = -1; l <= 1; l +=2)
                     {
-                        var vec =  new Vector3(i * k + Random.Range(-density, density) * rnd, j * l + Random.Range(-density, density) * rnd);
+                        var vec =  new Vector3(i * k + Random.Range(-density, density) * rnd + offset, j * l + Random.Range(-density, density) * rnd);
                         if ((vec - planet.position).sqrMagnitude > planet.localScale.x + atmosphere) {
-                            var go = Instantiate(prefab, vec, Quaternion.identity);
-                            var col = colors[Random.Range(0, colors.Length)];
-                            go.GetComponent<AsteroidProxy>().Value = new Asteroid() { resource = col.resource };
-                            go.GetComponent<SpriteRenderer>().color = col.color;
+                            var p = prefabs[Random.Range(0, prefabs.Length)];
+                            var go = Instantiate(p, vec, Quaternion.identity);
+                            go.name = "Asteroid (Spawned)";
                         }
                     }
                 }
