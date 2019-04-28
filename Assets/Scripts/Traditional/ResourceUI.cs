e2437f4e11d04c536d246a88c34bfb15c349b1f8
Aggrathon <antonbjo@gmail.com>
2019-04-28 12:54:39 +0300

    resource ui

---
+++ Assets/Scripts/Traditional/ResourceUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ResourceUI : MonoBehaviour
+{
+    public TextMeshProUGUI alumText;
+    public TextMeshProUGUI ironText;
+    public TextMeshProUGUI waterText;
+
+    int oldWater = -1;
+    int oldIron = -1;
+    int oldAlum = -1;
+
+    GameState state;
+
+    private void Start() {
+        state = FindObjectOfType<GameState>();
+    }
+
+    void Update()
+    {
+        if ((int)state.water != oldWater) {
+            oldWater = (int)state.water;
+            waterText.text = oldWater.ToString();
+        }
+        if (state.iron != oldIron) {
+            oldIron = (int)state.iron;
+            ironText.text = oldIron.ToString();
+        }
+        if (state.aluminium != oldAlum) {
+            oldAlum = state.aluminium;
+            alumText.text = oldAlum.ToString();
+        }
+    }
+}
